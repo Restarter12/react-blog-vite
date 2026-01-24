37522fec7a7e24c59c47231a25437827f5627235
Restarter <nikitovich60@bk.ru>
2026-01-24 16:06:46 +0300

    debug

--- vite-blog/vite.config.js
+++ vite-blog/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-blog/' // имя репозитория
+  base: '/vite-blog/' // имя репозитория
 })
